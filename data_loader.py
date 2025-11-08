254d2fcdd43cc5d8abd2261939a2bbbed7b1e472
mrkimhp94 <mrkimhp942@gmail.com>
2025-11-08 22:28:03 +0700

    update vnstock-> vnstock2

--- data_loader.py
+++ data_loader.py
@@ -1,19 +1,21 @@
-from vnstock import stock_historical_data
+from vnstock2 import Vnstock
 import pandas as pd
 from datetime import datetime, timedelta
 
 def load_data(symbol, lookback=200):
+    import pandas as pd
+    from datetime import datetime, timedelta
+
+    api = Vnstock()
+    stock = api.stock(symbol, 'stock')
+
     end = datetime.today().strftime("%Y-%m-%d")
     start = (datetime.today() - timedelta(days=lookback*2)).strftime("%Y-%m-%d")
 
-    df = stock_historical_data(
-        symbol,
-        start,
-        end,
-        resolution="1D"
-    )
+    df = stock.quote.history(start, end, interval='1D')
+
     df = df[['time', 'open', 'high', 'low', 'close', 'volume']]
     df['time'] = pd.to_datetime(df['time'])
     df = df.dropna()
-    df = df.tail(lookback)
-    return df
+    return df.tail(lookback)
+
