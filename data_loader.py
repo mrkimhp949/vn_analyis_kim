61c57a128db48a211d896d6d8ccff12cd0015de8
mrkimhp94 <mrkimhp942@gmail.com>
2025-11-08 22:39:49 +0700

    rollback

--- data_loader.py
+++ data_loader.py
@@ -1,21 +1,19 @@
-from vnstock2 import Vnstock
+from vnstock import stock_historical_data
 import pandas as pd
 from datetime import datetime, timedelta
 
 def load_data(symbol, lookback=200):
-    import pandas as pd
-    from datetime import datetime, timedelta
-
-    api = Vnstock()
-    stock = api.stock(symbol, 'stock')
-
     end = datetime.today().strftime("%Y-%m-%d")
     start = (datetime.today() - timedelta(days=lookback*2)).strftime("%Y-%m-%d")
 
-    df = stock.quote.history(start, end, interval='1D')
-
+    df = stock_historical_data(
+        symbol,
+        start,
+        end,
+        resolution="1D"
+    )
     df = df[['time', 'open', 'high', 'low', 'close', 'volume']]
     df['time'] = pd.to_datetime(df['time'])
     df = df.dropna()
-    return df.tail(lookback)
-
+    df = df.tail(lookback)
+    return df
